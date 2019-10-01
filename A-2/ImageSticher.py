4cabbc45a0461d04cdd44039fd1c751a840820d5
Chirag Manwani <cmkmanwani@gmail.com>
2019-10-01 11:21:01 +0530

    Added code for warping result and appending left image. Works OK but would be preferable if left is warped and appended to result

--- A-2/ImageSticher.py
+++ A-2/ImageSticher.py
@@ -1,7 +1,11 @@
 import cv2
 import os
 import numpy as np
+import natsort
+
 import matplotlib.pyplot as plt
+
+from helper import trim
 
 
 class ImageSticher:
@@ -21,12 +25,16 @@ class ImageSticher:
         img_dir
     ):
         files = os.listdir(img_dir)
+        files = natsort.natsorted(files)
+        print(files)
         images = []
         for file_ in files:
             if file_.split('.')[-1] not in ['jpg', 'png', 'jpeg']:
                 continue
             img_path = os.path.join(img_dir, file_)
-            img = cv2.imread(img_path, cv2.COLOR_BGR2RGB)
+            img = cv2.imread(img_path)
+            dim = (int(img.shape[1]/4), int(img.shape[0]/4))
+            img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
             images.append(img)
 
         self.images = images
@@ -35,7 +43,7 @@ class ImageSticher:
         self,
         img
     ):
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         sift = cv2.xfeatures2d.SIFT_create(self.n_keypoints, sigma=self.sigma)
         kps, des = sift.detectAndCompute(gray, None)
@@ -52,56 +60,68 @@ class ImageSticher:
         ratio,
         reprojThresh
     ):
-        # compute the raw matches and initialize the list of actual
-        # matches
-        matcher = cv2.DescriptorMatcher_create("BruteForce")
-        rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
-        matches = []
+        bf = cv2.BFMatcher(crossCheck=True)
+        matches = bf.match(featuresA, featuresB)
 
-        # loop over the raw matches
-        for m in rawMatches:
-            # ensure the distance is within a certain ratio of each
-            # other (i.e. Lowe's ratio test)
-            if len(m) == 2 and m[0].distance < m[1].distance * ratio:
-                matches.append((m[0].trainIdx, m[0].queryIdx))
+        matches.sort(key=lambda x: x.distance, reverse=False)
+        # top_matches = matches[:4]
+        # print(matches[0].distance, matches[1].distance)
+        ptsA = np.float32([kpsA[match.queryIdx] for match in matches[:10]])
+        ptsB = np.float32([kpsB[match.trainIdx] for match in matches[:10]])
 
-        if len(matches) > 4:
-            # construct the two sets of points
-            ptsA = np.float32([kpsA[i] for (_, i) in matches])
-            ptsB = np.float32([kpsB[i] for (i, _) in matches])
+        (H, status) = cv2.findHomography(ptsA, ptsB,
+                                         cv2.RANSAC,
+                                         reprojThresh)
 
-            # compute the homography between the two sets of points
-            (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,
-                                             reprojThresh)
-
-            # return the matches along with the homograpy matrix
-            # and status of each matched point
-            return (matches, H, status)
+        return (matches, H, status)
 
     def stitch(
         self
     ):
-        img1 = self.images[0]
-        img2 = self.images[1]
+        n = len(self.images)
+        result = self.stitch_2(self.images[-1], self.images[-2])
+        for i in range(n-3, -1, -1):
+            print("i=", i)
+            result = self.stitch_2(result, self.images[i])
+        return result
 
-        kps1, des1 = self.__get_keypoints__(img1)
-        kps2, des2 = self.__get_keypoints__(img2)
+    def stitch_2(
+        self,
+        img0,
+        img1
+    ):
+        img = []
+        img.append(img0)
+        img.append(img1)
 
-        M = self.__matchKeypoints__(kps1, kps2,
-                                    des1, des2,
-                                    0.75, 4)
+        kps0, des0 = self.__get_keypoints__(img[0])
+        kps1, des1 = self.__get_keypoints__(img[1])
+
+        M = self.__matchKeypoints__(kps0, kps1,
+                                    des0, des1,
+                                    0.25, 4)
 
         (matches, H, status) = M
-        result = cv2.warpPerspective(img1, H,
-                                     (img1.shape[1]+img2.shape[1], img1.shape[0]))
-        result[0:img2.shape[0], 0:img2.shape[1]] = img2
+        inv_H = np.linalg.inv(H)
 
-        # vis = drawMatches(imageA, imageB, kpsA, kpsB, matches,
-        #                        status)
+        x0 = kps0[matches[0].queryIdx][0]
+        x1 = kps1[matches[1].trainIdx][0]
 
-        # return a tuple of the stitched image and the
-        # visualization
+        overlap = int(abs(x0 - x1))
+        result_temp = cv2.warpPerspective(img[1],
+                                          inv_H,
+                                          (img[1].shape[1], img[1].shape[0]))
+
+        plt.imshow(cv2.cvtColor(result_temp, cv2.COLOR_BGR2RGB))
+        plt.show()
+        result = cv2.warpPerspective(img[0], H,
+                                     (img[0].shape[1]+img[1].shape[1], img[0].shape[0]))
+        # plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+        # plt.show()
+
+        result[0:img[1].shape[0], 0:img[1].shape[1]] = img[1]
+        result = trim(result)
         plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-        plt.imsave('out.jpg', result)
-
+        plt.show()
+        # plt.imsave('out.jpg', cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
         return result
