f53cc19eb9acbf3e2d87a48fddf65d30694aed71
Chirag Manwani <cmkmanwani@gmail.com>
2019-10-01 10:20:08 +0530

    Added helper functions file. Trim to remove Black Portion in images

---
+++ A-2/helper.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import cv2 as cv
+
+def trim(frame):
+    if not np.sum(frame[0]):
+        return trim(frame[1:])
+    if not np.sum(frame[-1]):
+        return trim(frame[:-2])
+    if not np.sum(frame[:,0]):
+        return trim(frame[:,1:])
+    if not np.sum(frame[:,-1]):
+        return trim(frame[:,:-2])
+    return frame
