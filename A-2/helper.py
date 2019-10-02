7a6cd748caa475c0f3399f10958bd0d146bd8304
Chirag Manwani <cmkmanwani@gmail.com>
2019-10-02 10:18:11 +0530

    Added hist. eq to helper

--- A-2/helper.py
+++ A-2/helper.py
@@ -1,13 +1,24 @@
 import numpy as np 
 import cv2 as cv
 
+
 def trim(frame):
     if not np.sum(frame[0]):
         return trim(frame[1:])
     if not np.sum(frame[-1]):
         return trim(frame[:-2])
-    if not np.sum(frame[:,0]):
-        return trim(frame[:,1:])
-    if not np.sum(frame[:,-1]):
-        return trim(frame[:,:-2])
+    if not np.sum(frame[:, 0]):
+        return trim(frame[:, 1:])
+    if not np.sum(frame[:, -1]):
+        return trim(frame[:, :-2])
     return frame
+
+
+def hisEqulColor(img):
+    ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    channels = cv2.split(ycrcb)
+    print len(channels)
+    cv2.equalizeHist(channels[0], channels[0])
+    cv2.merge(channels, ycrcb)
+    cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR, img)
+    return img
