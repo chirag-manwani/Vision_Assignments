3f6767d9dc0a4329e5d817f08a6a0ae49a0f0f2e
Chirag Manwani <cmkmanwani@gmail.com>
2020-01-16 18:34:10 +0530

    A-3 final touches

--- A-3/helper.py
+++ A-3/helper.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import imutils
+import math
 
 
 def calibrate(
@@ -41,37 +43,154 @@ def record(
 
 
 def get_keypoints(
-    self,
     img
 ):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    sift = cv2.xfeatures2d.SIFT_create(sigma=self.sigma)
+    sift = cv2.xfeatures2d.SIFT_create()
     kps, des = sift.detectAndCompute(gray, None)
 
-    kps = np.float32([kp.pt for kp in kps])
+    # kps = np.float32([kp.pt for kp in kps])
     return kps, des
 
 
 def matchKeypoints(
-    self,
     kpsA,
     kpsB,
     featuresA,
     featuresB,
-    ratio,
     reprojThresh
 ):
     bf = cv2.BFMatcher(crossCheck=True)
     matches = bf.match(featuresA, featuresB)
 
     matches.sort(key=lambda x: x.distance, reverse=False)
+    kpsA = np.float32([kp.pt for kp in kpsA])
+    kpsB = np.float32([kp.pt for kp in kpsB])
 
-    ptsA = np.float32([kpsA[match.queryIdx] for match in matches[:20]])
-    ptsB = np.float32([kpsB[match.trainIdx] for match in matches[:20]])
+    ptsA = np.float32([kpsA[match.queryIdx] for match in matches])
+    ptsB = np.float32([kpsB[match.trainIdx] for match in matches])
 
     (H, status) = cv2.findHomography(ptsA, ptsB,
-                                        cv2.RANSAC,
-                                        reprojThresh)
-    A = cv2.getAffineTransform(ptsA[:3], ptsB[:3])
-    return (matches, H, A)
+                                     cv2.RANSAC,
+                                     reprojThresh)
+    # A = cv2.getAffineTransform(ptsA[:3], ptsB[:3])
+    return (matches, H, ptsA, ptsB)
+
+
+def projection_matrix(camera_parameters, homography):
+    """
+    From the camera calibration matrix and the estimated homography
+    compute the 3D projection matrix
+    """
+    # Compute rotation along the x and y axis as well as the translation
+    homography = homography * (-1)
+    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
+    col_1 = rot_and_transl[:, 0]
+    col_2 = rot_and_transl[:, 1]
+    col_3 = rot_and_transl[:, 2]
+    # normalise vectors
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rot_1 = col_1 / l
+    rot_2 = col_2 / l
+    translation = col_3 / l
+    # compute the orthonormal basis
+    c = rot_1 + rot_2
+    p = np.cross(rot_1, rot_2)
+    d = np.cross(c, p)
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_3 = np.cross(rot_1, rot_2)
+    # finally, compute the 3D projection matrix from the model to the current frame
+    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+    return np.dot(camera_parameters, projection)
+
+
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        """Loads a Wavefront OBJ file. """
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'v':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(map(float, values[1:3]))
+            #elif values[0] in ('usemtl', 'usemat'):
+                #material = values[1]
+            #elif values[0] == 'mtllib':
+                #self.mtl = MTL(values[1])
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                #self.faces.append((face, norms, texcoords, material))
+                self.faces.append((face, norms, texcoords))
+
+
+def render(img, obj, projection, model, color=False):
+    vertices = obj.vertices
+    # print(dir(vertices[0]))
+    scale_matrix = np.eye(3) * 3
+    h, w = model.shape[:2]
+
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = [list(vertices[vertex - 1]) for vertex in face_vertices]
+        while [] in points:
+            points.remove([])
+        if(len(points) == 0):
+            continue
+        points = np.array(points)
+        # points = np.array([list(p) for p in points], dtype='float32')
+        # print(points)
+        points = np.dot(points, scale_matrix)
+        rot = np.eye(3)
+        rot[2, 2] = 0
+        rot[1, 1] = 0
+        rot[1, 2] = -1
+        rot[2, 1] = 1
+        points = np.dot(points, rot)
+        # points = scale_matrix @ points
+        # render model in the middle of the reference surface. To do so,
+        # model points must be displaced
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1] # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
+
+
+if __name__ == "__main__":
+    record('p3.avi')
--- A-3/main.py
+++ A-3/main.py
@@ -2,9 +2,13 @@ import numpy as np
 import sys
 import os
 import cv2
-
+import pywavefront
 from helper import calibrate
 from helper import record
+from helper import get_keypoints
+from helper import matchKeypoints
+from helper import projection_matrix
+from helper import OBJ, render
 
 data_base_path = '../../Assignment_data/A-3'
 
@@ -13,29 +17,145 @@ def checkerBoard(args):
     objp = np.zeros((4*6, 3), np.float32)
     objp[:, :2] = np.mgrid[0:6, 0:4].T.reshape(-1, 2)
 
-    cap = cv2.VideoCapture(args[0])
-
+    cap = cv2.VideoCapture('abc.avi')
+    # print(objp, objp.shape, type(objp))
     while(True):
+        # print('here')
         ret, frame = cap.read()
         if(not ret):
             break
         frame_, corners = calibrate(frame, (6, 4))
+        
         if(corners is not None):
+            print('Corners', corners.shape, objp.shape)
             ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera([objp],
                                                                [corners],
                                                                frame_.shape[:2][::-1],
                                                                None,
                                                                None)
-            print(mtx)
-            print(dist)
+            # print(mtx)
+            # print(dist)
 
         cv2.imshow('frame', frame_)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
+def part3(
+    args,
+    vis=False
+):
+    img_m = cv2.imread(args[1])
+    kpsm, desm = get_keypoints(img_m)
+
+    cap = cv2.VideoCapture(args[0])
+    
+    while(True):
+        ret, frame = cap.read()
+        if(not ret):
+            break
+        # frame = cv2.flip(frame, 1)
+
+        kpsf, desf = get_keypoints(frame)
+        matches, H, ptsm, ptsf = matchKeypoints(kpsm, kpsf, desm, desf, 5)
+
+        # Reshaping
+        b = np.zeros((ptsm.shape[0], ptsm.shape[1]+1))
+        b[:,:-1] = ptsm
+        ptsm = np.array(b, dtype='float32')
+        ptsf = np.reshape(ptsf, (ptsf.shape[0], 1, 2))
+        # Reshaping end
+
+        if H is not None:
+            ret, c_mat, dist, rvecs, tvecs = cv2.calibrateCamera([ptsm], [ptsf], frame.shape[:2][::-1], None, None)
+            size = frame.shape[:2]
+            focal_length = size[1] 
+            center = (size[1]/2, size[0]/2) 
+            camera_matrix = np.array( 
+                            [[ focal_length ,      0       , center[0] ], 
+                             [      0       , focal_length , center[1] ], 
+                             [      0       ,      0       ,     1     ]], dtype = "double"
+                             )
+            p_mat = projection_matrix(camera_matrix, H)
+            if (vis):
+                h, w = img_m.shape[:2]
+                pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(pts, H)
+                img2 = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA) 
+                cv2.imshow('frame', img2)
+                # cv2.waitKey(0)
+            # obj = pywavefront.Wavefront('pc/police-car.obj')
+            
+            obj = OBJ('lp/lp.obj')
+            frame_ = render(frame, obj, p_mat, img_m, color=False)
+            cv2.imshow('frame', frame_)
+            # cv2.waitKey(0)
+            # print(cam_mat)
+            
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def part4(
+    args,
+    vis=False
+):
+    img_m = cv2.imread(args[1])
+    kpsm, desm = get_keypoints(img_m)
+
+    cap = cv2.VideoCapture(0)
+    
+    while(True):
+        ret, frame = cap.read()
+        if(not ret):
+            break
+        # frame = cv2.flip(frame, 1)
+
+        kpsf, desf = get_keypoints(frame)
+        matches, H, ptsm, ptsf = matchKeypoints(kpsm, kpsf, desm, desf, 5)
+
+        # Reshaping
+        b = np.zeros((ptsm.shape[0], ptsm.shape[1]+1))
+        b[:,:-1] = ptsm
+        ptsm = np.array(b, dtype='float32')
+        ptsf = np.reshape(ptsf, (ptsf.shape[0], 1, 2))
+        # Reshaping end
+
+        if H is not None:
+            ret, c_mat, dist, rvecs, tvecs = cv2.calibrateCamera([ptsm], [ptsf], frame.shape[:2][::-1], None, None)
+            size = frame.shape[:2]
+            focal_length = size[1] 
+            center = (size[1]/2, size[0]/2) 
+            camera_matrix = np.array( 
+                            [[ focal_length ,      0       , center[0] ], 
+                             [      0       , focal_length , center[1] ], 
+                             [      0       ,      0       ,     1     ]], dtype = "double"
+                             )
+            p_mat = projection_matrix(camera_matrix, H)
+            if (vis):
+                h, w = img_m.shape[:2]
+                pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(pts, H)
+                img2 = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA) 
+                cv2.imshow('frame', img2)
+                # cv2.waitKey(0)
+            # obj = pywavefront.Wavefront('pc/police-car.obj')
+            
+            obj = OBJ('lp/lp.obj')
+            frame_ = render(frame, obj, p_mat, img_m, color=False)
+            cv2.imshow('frame', frame_)
+            # cv2.waitKey(0)
+            # print(cam_mat)
+            
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if len(args) != 1:
+    if len(args) < 1:
         exit()
-    # checkerBoard(args)
+    # checkerBoard('abc.avi')
+    part3(args)
